cf1eb2bc840fb8acfb82be8d7ab3f320473723b5
Tahabz <mtahabaz@gmail.com>
2022-03-15 16:37:26 +0100

    create ex00 folder

---
+++ day05/ex00/Bureaucrat.hpp
@@ -0,0 +1,39 @@
+#if !defined(BUREAUCRAT_HPP)
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+
+class Bureaucrat
+{
+public:
+	Bureaucrat(void);
+	Bureaucrat(const std::string &name,
+			   int grade) throw(Bureaucrat::GradeTooHighException, Bureaucrat::GradeTooLowException);
+	Bureaucrat(const Bureaucrat &src);
+	Bureaucrat &operator=(const Bureaucrat &src);
+	~Bureaucrat();
+	std::string getName() const;
+	int getGrade() const;
+	void incrementGrade() throw(Bureaucrat::GradeTooHighException);
+	void decrementGrade() throw(Bureaucrat::GradeTooLowException);
+
+	class GradeTooHighException : public std::exception
+	{
+	public:
+		const char *what() const throw();
+	};
+
+	class GradeTooLowException : public std::exception
+	{
+	public:
+		const char *what() const throw();
+	};
+
+private:
+	const std::string _name;
+	int _grade;
+};
+
+std::ostream &operator<<(std::ostream &o, Bureaucrat &bc);
+
+#endif // BUREAUCRAP_HPP
